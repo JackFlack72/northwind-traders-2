a22535dc03509de1710a564c0442154b57985906
lucas.rappatta24@outlook.com <lucas.rappatta24@outlook.com>
2025-12-03 10:40:08 -0600

    add security strings

---
+++ src/main/java/com/northwind/Program.java
@@ -0,0 +1,10 @@
+package com.northwind;
+
+public class Program {
+    public static void main(String[] args) {
+        String username = args[0];
+        String password = args[1];
+        String url = "jdbc:mysql://localhost:3306/northwind";
+
+    }
+}
