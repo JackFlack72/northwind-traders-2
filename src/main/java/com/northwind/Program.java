8039716c11465eef6120702600defc03aedc7520
lucas.rappatta24@outlook.com <lucas.rappatta24@outlook.com>
2025-12-03 12:28:22 -0600

    catch up and add DAO

--- src/main/java/com/northwind/Program.java
+++ src/main/java/com/northwind/Program.java
@@ -1,10 +1,24 @@
 package com.northwind;
 
+import com.northwind.data.CustomerDao;
+import org.apache.commons.dbcp2.BasicDataSource;
+
+
 public class Program {
     public static void main(String[] args) {
         String username = args[0];
         String password = args[1];
-        String url = "jdbc:mysql://localhost:3306/northwind";
+        String url = "jdbc:mysql://localhost:3306/Northwind";
+
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+
+
+        CustomerDao customerDao = new CustomerDao(dataSource);
+
+
 
     }
 }
